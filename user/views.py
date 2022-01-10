a75383c749d55d879c06e674ce66c8242aeb9f48
Ilya Pankov <iluxaan@mail.ru>
2022-01-10 18:15:50 +0300

    refactor: commit def lk and her url path

--- user/views.py
+++ user/views.py
@@ -15,13 +15,14 @@ def login(request):
             user = auth.authenticate(username=email, password=password)
             if user and user.is_active:
                 auth.login(request, user)
-                return HttpResponseRedirect(reverse('lk'))
+                # return HttpResponseRedirect(reverse('lk')) - вернуть, когда будет создано основное приложение
+                return HttpResponseRedirect(reverse(''))
     else:
         form = UserLoginForm()
     context = {'title': 'Login',
                'form': form}
     return render(request, 'user/login.html', context)
 
-
-def lk(request):
-    return render(request, 'user/lk.html')
+# Вернуть, когда будет создано основное приложение
+# def lk(request):
+#     return render(request, 'user/lk.html')
