6949ef02b0394e2e9af4d364b2734096d272de87
KokoulinKV <jelikavar@gmail.com>
2022-01-11 00:03:22 +0700

    added user authentication

--- user/urls.py
+++ user/urls.py
@@ -5,6 +5,7 @@ from user.views import login #, lk
 app_name = 'user'
 
 urlpatterns = [
-    path('login/', login, name='login'),
-    # path('lk/', lk, name='lk')
+    path('', login, name='login'),
+    # path('lk/', lk, name='lk'),
+    # path('logout/', logout, name='logout'),
 ]
--- user/views.py
+++ user/views.py
@@ -4,24 +4,27 @@ from django.shortcuts import render
 from django.urls import reverse
 
 from user.forms import UserLoginForm
+from user.models import User
 
 
 def login(request):
     if request.method == 'POST':
         form = UserLoginForm(data=request.POST)
-        if form.is_valid():
-            email = request.POST['email']
-            password = request.POST['password']
-            user = auth.authenticate(username=email, password=password)
-            if user and user.is_active:
-                auth.login(request, user)
-                # return HttpResponseRedirect(reverse('lk')) - вернуть, когда будет создано основное приложение
-                return HttpResponseRedirect(reverse(''))
+        email = request.POST['email']
+        username = User.objects.get(email=email)
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user and user.is_active:
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('user:lk'))
     else:
         form = UserLoginForm()
     context = {'title': 'Login',
                'form': form}
     return render(request, 'user/login.html', context)
+
+
+
 
 # Вернуть, когда будет создано основное приложение
 # def lk(request):
