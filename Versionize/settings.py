ad2c001e4c9af7d4b3c3ad91b9ff6b1c57a20fee
KokoulinKV <jelikavar@gmail.com>
2022-01-11 00:01:25 +0700

    add database settings and auth_model = user.User

--- Versionize/settings.py
+++ Versionize/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Versionize.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'versionize',
+        'USER': 'versionize',
+        'PASSWORD': 'versionize',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -124,3 +128,5 @@ STATICFILES_DIRS=(BASE_DIR/'static',)
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL='user.User'
