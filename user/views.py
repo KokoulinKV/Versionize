dd39236ff5256286fb389af6db0def3ff72bd799
KokoulinKV <jelikavar@gmail.com>
2022-01-11 21:47:31 +0700

    except 'User matching query does not exist.'

--- user/views.py
+++ user/views.py
@@ -8,23 +8,28 @@ from user.models import User
 
 
 def login(request):
-    if request.method == 'POST':
-        form = UserLoginForm(data=request.POST)
-        email = request.POST['email']
-        username = User.objects.get(email=email)
-        password = request.POST['password']
-        user = auth.authenticate(username=username, password=password)
-        if user and user.is_active:
-            auth.login(request, user)
-            return HttpResponseRedirect(reverse('main:index'))
-    else:
+    try:
+        user_error = False
+        if request.method == 'POST':
+            form = UserLoginForm(data=request.POST)
+            email = request.POST['email']
+            username = User.objects.get(email=email)
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('main:index'))
+        else:
+            form = UserLoginForm()
+    except:
         form = UserLoginForm()
+        user_error = True
     context = {'title': 'Login',
-               'form': form}
+               'form': form,
+               'user_error':user_error}
     return render(request, 'user/login.html', context)
 
 
 def logout(request):
     auth.logout(request)
     return HttpResponseRedirect(reverse('user:login'))
-
