ccf228f37d2ae15ccc4f6bd781a319343a6c971b
Ilya Pankov <iluxaan@mail.ru>
2022-01-11 12:47:55 +0300

    feat: add view for new templates

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from main.views import index
+# !Дать нормальные названия, передалть в cbv
+from main.views import index, document, company, section, total
 
 app_name = 'main'
 
@@ -8,4 +9,10 @@ urlpatterns = [
     path('', index, name='index'),
     # path('lk/', lk, name='lk'),
     # path('logout/', logout, name='logout'),
+
+    # Для проверки отображения
+    path('company/', company, name='company'),
+    path('document/', document, name='document'),
+    path('section/', section, name='section'),
+    path('total/', total, name='total'),
 ]
--- main/views.py
+++ main/views.py
@@ -5,3 +5,17 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'main/lk.html')
 
+
+# Проверка отображения новых шаблонов
+def company(request):
+    return render(request, 'main/company.html')
+
+def document(request):
+    return render(request, 'main/document.html')
+
+def section(request):
+    return render(request, 'main/section.html')
+
+def total(request):
+    return render(request, 'main/total.html')
+
