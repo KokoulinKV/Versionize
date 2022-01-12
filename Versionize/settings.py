4a0acf46943d468e7aca48ac972b2eb9cadc380e
AlexKovyazin <KovyazinAlexander@yandex.ru>
2022-01-12 16:04:06 +0300

    add: media root

--- Versionize/settings.py
+++ Versionize/settings.py
@@ -133,11 +133,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=(BASE_DIR/'static',)
+STATICFILES_DIRS = (BASE_DIR/'static',)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL='user.User'
+AUTH_USER_MODEL = 'user.User'
