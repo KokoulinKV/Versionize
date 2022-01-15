09f671a16088df4bc9d0cdc267a8a8e79344b1f5
KokoulinKV <jelikavar@gmail.com>
2022-01-16 03:27:22 +0700

    added new urls

--- admins/urls.py
+++ admins/urls.py
@@ -1,19 +1,27 @@
 from django.urls import path
 
-from admins.views import UserCreateView, UserListView, UserAdminView, UserAdminDelete, UserAdminRehub, CompanyListView, \
-    CompanyAdminView, CompanyCreateView, CompanyAdminDelete, CompanyAdminDeleteMessage
+from admins.views import UserCreateView, UserListView, UserEditView, UserDeleteView, UserRehubView, CompanyListView, \
+    CompanyEditView, CompanyCreateView, CompanyAdminDelete, CompanyAdminDeleteMessage, UserAddInfoView, \
+    UserInfoListView, UserInfoEdit
 
 app_name = 'admins'
 
 urlpatterns = [
+    # Users
     path('', UserListView.as_view(), name='index'),
     path('users/', UserCreateView.as_view(), name='admins_users_create'),
-    path('users_update/<int:pk>', UserAdminView.as_view(), name='admins_users_update'),
-    path('users_delete/<int:pk>', UserAdminDelete.as_view(), name='admins_users_delete'),
-    path('users_rehub/<int:pk>', UserAdminRehub.as_view(), name='admins_users_rehub'),
+    path('users_update/<int:pk>', UserEditView.as_view(), name='admins_users_update'),
+    path('users_delete/<int:pk>', UserDeleteView.as_view(), name='admins_users_delete'),
+    path('users_rehub/<int:pk>', UserRehubView.as_view(), name='admins_users_rehub'),
 
+    # User's Info
+    path('users_info/', UserInfoListView.as_view(), name='admins_usersinfo'),
+    path('users_addinfo/', UserAddInfoView.as_view(), name='admins_addinfo'),
+    path('users_infoedit/<int:pk>', UserInfoEdit.as_view(), name='admins_usersinfo_edit'),
+
+    # Companies
     path('companies/', CompanyListView.as_view(), name='admins_companies'),
-    path('companies_update/<int:pk>', CompanyAdminView.as_view(), name='admins_companies_update'),
+    path('companies_update/<int:pk>', CompanyEditView.as_view(), name='admins_companies_update'),
     path('companies_create/', CompanyCreateView.as_view(), name='admins_companies_create'),
     path('companies_delete_message/<int:pk>', CompanyAdminDeleteMessage.as_view(),
          name='admins_companies_delete_message'),
