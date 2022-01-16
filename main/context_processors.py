144549ff1cb141439a1174e6f97cafb6638b449b
AlexKovyazin <KovyazinAlexander@yandex.ru>
2022-01-16 21:01:23 +0300

    add: context processor for active project info

---
+++ main/context_processors.py
@@ -0,0 +1,19 @@
+from main.models import Project
+
+
+def active_project_info(request):
+    # При новой сессии ключ 'active_project_id' отсутствует
+    # TODO откорректировать с учётом прав доступа
+    if 'active_project_id' not in request.session.keys():
+        first_project = Project.objects.first()
+        request.session['active_project_id'] = first_project.id
+    project = Project.objects.filter(id=request.session['active_project']).first()
+    request.session['active_project_id'] = project.id
+
+    return {
+        'active_project_id': project.id,
+        'active_project_code': project.code,
+        'active_project_name': project.name,
+        'active_project_exp_date': project.exp_date,
+        'active_project_next_upload': project.next_upload,
+    }
