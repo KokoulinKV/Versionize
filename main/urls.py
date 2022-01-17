683b28329fe1fa47244b14004f34b9f72cab7157
Ilya Pankov <iluxaan@mail.ru>
2022-01-17 15:02:24 +0300

    fix,refactor: fix merge errors

--- main/urls.py
+++ main/urls.py
@@ -1,21 +1,26 @@
 from django.urls import path
 
 # !Дать нормальные названия, передалть в cbv
-from main.views import index, document, company, section, total, index2, document2, company2, section2, total2
+from main.views import TotalListView, CompanyListView, SectionDetailView, DocumentDetailView, Index, document2, company2, section2, total2
 
 app_name = 'main'
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', Index.as_view(), name='index'),
+    path('companies/', CompanyListView.as_view(), name='companies'),
+    # path('company/<int:pk>', CompanyDetailView.as_view(), name='company'),
+    path('document/<int:pk>', DocumentDetailView.as_view(), name='document'),
+    path('section/<int:pk>', SectionDetailView.as_view(), name='section'),
+    path('total/', TotalListView.as_view(), name='total'),
     # path('lk/', lk, name='lk'),
     # path('logout/', logout, name='logout'),
 
     # Для проверки отображения
-    path('company/', company, name='company'),
-    path('document/', document, name='document'),
-    path('section/', section, name='section'),
-    path('total/', total, name='total'),
-    path('new_lk/', index2, name='index2'),
+    # path('company/', company, name='company'),
+    # path('document/', document, name='document'),
+    # path('section/', section, name='section'),
+    # path('total/', total, name='total'),
+    # path('new_lk/', index2, name='index2'),
     path('company2/', company2, name='company2'),
     path('document2/', document2, name='document2'),
     path('section2/', section2, name='section2'),
--- main/views.py
+++ main/views.py
@@ -1,41 +1,74 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from main.models import Section, Company, Document
 
 
-def index(request):
-    return render(request, 'main/lk.html')
+class Index(TemplateView):
+    template_name = 'main/lk2.html'
 
 
-# Проверка отображения новых шаблонов
-def index2(request):
-    return render(request, 'main/lk2.html')
+class TotalListView(LoginRequiredMixin, ListView):
+    model = Section
+    template_name = 'main/total.html'
+
+    def get_queryset(self):
+        queryset = self.model.objects.filter(
+            project_id=self.request.session['active_project_id'])
+        ordering = self.get_ordering()
+        if ordering:
+            if isinstance(ordering, str):
+                ordering = (ordering, )
+            queryset = queryset.order_by(*ordering)
+
+        return queryset
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Versionize - Сводная таблица проекта'
+        return context
 
 
-def company(request):
-    return render(request, 'main/company.html')
+class SectionDetailView(LoginRequiredMixin, DetailView):
+    model = Section
+    template_name = 'main/section.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Versionize - Раздел'
+        return context
+
+
+class CompanyListView(LoginRequiredMixin, ListView):
+    model = Company
+    template_name = 'main/companies.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Versionize - Компании'
+        return context
+
+
+class DocumentDetailView(LoginRequiredMixin, DetailView):
+    model = Document
+    template_name = 'main/document.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Versionize - Документ'
+        return context
 
 
 def company2(request):
     return render(request, 'main/company2.html')
 
 
-def document(request):
-    return render(request, 'main/document.html')
-
-
 def document2(request):
     return render(request, 'main/document2.html')
 
 
-def section(request):
-    return render(request, 'main/section.html')
-
-
 def section2(request):
     return render(request, 'main/section2.html')
-
-
-def total(request):
-    return render(request, 'main/total.html')
 
 
 def total2(request):
