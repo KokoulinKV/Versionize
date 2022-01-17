4dfe9b053f4c78e6bf3f1fc0fa90ce1f23adef2d
AlexKovyazin <KovyazinAlexander@yandex.ru>
2022-01-17 22:04:17 +0300

    refactor: filldb script

--- main/management/commands/filldb.py
+++ main/management/commands/filldb.py
@@ -1,16 +1,8 @@
 import sqlite3
 import os
-# from Versionize.settings import BASE_DIR
+from Versionize.settings import BASE_DIR
 from django.core.management.base import BaseCommand
 from django.conf import settings
-
-# conn = sqlite3.connect(os.path.join(BASE_DIR, 'db.sqlite3'))
-# cursor = conn.cursor()
-
-# with open('versionize_inserts_1.1.sql') as file:
-#     for sql_command in file.readlines():
-#         cursor.execute(f'{sql_command}')
-#         conn.commit()
 
 
 class Command(BaseCommand):
@@ -19,9 +11,19 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         conn = sqlite3.connect(os.path.join(settings.BASE_DIR, 'db.sqlite3'))
-        file_path = os.path.join(settings.BASE_DIR, 'main/management/commands/versionize_inserts_1.1.sql')
+        file_path = os.path.join(settings.BASE_DIR, 'main/management/commands/versionize_inserts.sql')
         cursor = conn.cursor()
         with open(file_path) as file:
             for sql_command in file.readlines():
                 cursor.execute(f'{sql_command}')
                 conn.commit()
+
+
+if __name__ == "__main__":
+    conn = sqlite3.connect(os.path.join(BASE_DIR, 'db.sqlite3'))
+    cursor = conn.cursor()
+
+    with open('versionize_inserts.sql') as file:
+        for sql_command in file.readlines():
+            cursor.execute(f'{sql_command}')
+            conn.commit()
