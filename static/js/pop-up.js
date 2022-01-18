ecea51d5bb124f55db98ba277247063e39f5dc23
Ilya Pankov <iluxaan@mail.ru>
2022-01-18 13:39:37 +0300

    fix(pop-up.js): fix func bug

--- static/js/pop-up.js
+++ static/js/pop-up.js
@@ -1,38 +1,37 @@
-let popup_doc = document.getElementById('popup-document')
-let popup_section = document.getElementById('popup-section')
-let popup_project = document.getElementById('popup-project')
+let popup_doc = document.getElementById('add-document')
+let popup_section = document.getElementById('add-section')
+let popup_project = document.getElementById('add-project')
 let popup_docCloseIcon = document.getElementById('popup__close_doc')
 let popup_secCloseIcon = document.getElementById('popup__close_sec')
 let popup_proCloseIcon = document.getElementById('popup__close_pro')
 
-popup_doc.addEventListener('click', () => {
-    let popUp = document.getElementById('popup-document')
-    popUp.classList.toggle('popup_open')
+const docum = document.getElementById('popup-document')
+const sect = document.getElementById('popup-section')
+const project = document.getElementById('popup-project')
+
+popup_doc.addEventListener('click', (e) => {
+
+    docum.classList.toggle('popup_open')
 })
 
 popup_docCloseIcon.addEventListener('click', (e) => {
-    let popUp = document.getElementById('popup-document')
-    popUp.classList.toggle('popup_open')
+    docum.classList.toggle('popup_open')
 })
 
-popup_section.addEventListener('click', () => {
-    let popUp = document.getElementById('popup-section')
-    popUp.classList.toggle('popup_open')
+popup_section.addEventListener('click', (e) => {
+    sect.classList.toggle('popup_open')
 })
 
 popup_secCloseIcon.addEventListener('click', (e) => {
-    let popUp = document.getElementById('popup-section')
-    popUp.classList.toggle('popup_open')
+    sect.classList.toggle('popup_open')
 })
 
-popup_project.addEventListener('click', () => {
-    let popUp = document.getElementById('popup-project')
-    popUp.classList.toggle('popup_open')
+popup_project.addEventListener('click', (e) => {
+    project.classList.toggle('popup_open')
 })
 
 popup_proCloseIcon.addEventListener('click', (e) => {
-    let popUp = document.getElementById('popup-project')
-    popUp.classList.toggle('popup_open')
+    project.classList.toggle('popup_open')
 })
 
 
