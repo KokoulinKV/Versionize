156217791953e925e17774de8c8cc8646800d98e
KokoulinKV <jelikavar@gmail.com>
2022-01-19 22:51:42 +0700

    add form for upload document

---
+++ main/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+
+from main.models import Document, Section
+
+
+class DocumentForm(forms.ModelForm):
+    name = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form__input', 'placeholder': 'Введите название отдела'}))
+    doc_path = forms.FileField(widget=forms.FileInput(attrs={'class': 'form__input'}), required=False)
+    section = forms.ModelChoiceField(queryset=Section.objects.all())
+    note =forms.CharField(widget=forms.Textarea(attrs={'class': 'form__textarea'}))
+
+    class Meta:
+        model = Document
+        fields = ('status', 'name', 'doc_path', 'section', 'note',)
+
