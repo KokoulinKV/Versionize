5ec27e1a76eb01f10c9ed2301325cee161d32805
Ilya Pankov <iluxaan@mail.ru>
2022-01-21 18:15:47 +0300

    feat: add russian name for main models

--- main/models.py
+++ main/models.py
@@ -7,13 +7,16 @@ from user.models import User, Company
 
 
 class Project(models.Model):
-    code = models.CharField(max_length=64, verbose_name='Шифр проекта')
-    name = models.CharField(max_length=512,
-                            verbose_name='Наименование проекта')
-    created_at = models.DateTimeField(auto_now=True)
+    code = models.CharField(max_length=64, verbose_name='Шифр')
+    name = models.CharField(max_length=512, verbose_name='Наименование')
+    created_at = models.DateTimeField(auto_now=True,
+                                      verbose_name='Дата создания')
     exp_date = models.DateField()
     next_upload = models.DateField()
-    admin = models.ForeignKey(User, db_index=True, on_delete=models.CASCADE)
+    admin = models.ForeignKey(User,
+                              db_index=True,
+                              on_delete=models.CASCADE,
+                              verbose_name='Создал')
 
     def get_projects(self):
         return self.objects.all()
@@ -27,15 +30,21 @@ class Project(models.Model):
 
 
 class Section(models.Model):
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    name = models.CharField(max_length=64)
-    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project,
+                                on_delete=models.CASCADE,
+                                verbose_name='Проект')
+    name = models.CharField(max_length=64, verbose_name='Наименование')
+    company = models.ForeignKey(Company,
+                                on_delete=models.CASCADE,
+                                verbose_name='Организация')
     responsible = models.ForeignKey(User,
                                     db_index=True,
-                                    on_delete=models.CASCADE)
+                                    on_delete=models.CASCADE,
+                                    verbose_name='Ответственный')
     expert = models.ForeignKey(User,
                                on_delete=models.CASCADE,
-                               related_name='expert_id')
+                               related_name='expert_id',
+                               verbose_name='Эксперт')
 
     def __str__(self):
         return self.name
@@ -66,19 +75,25 @@ class Document(models.Model):
         ('Положительное', 'Положительное'),
         ('Отрицательное', 'Отрицательное'),
     )
-    name = models.CharField(max_length=128)
-    doc_path = models.FileField(upload_to='main_docs')
-    section = models.ForeignKey(Section, on_delete=models.CASCADE)
+    name = models.CharField(max_length=128, verbose_name='Наименование')
+    doc_path = models.FileField(upload_to='main_docs', verbose_name='Путь')
+    section = models.ForeignKey(Section,
+                                on_delete=models.CASCADE,
+                                verbose_name='Раздел')
     # TODO Предусмотреть автоматическое заполнение ячейки version.
     #  Если ранее был загружен документ в раздел с идентичным section_id,
     #  то берём номер версии предыдущего + 1, если нет - 1.
-    version = models.IntegerField()
-    variation = models.IntegerField()
+    version = models.IntegerField(verbose_name='Версия')
+    variation = models.IntegerField(verbose_name='Изменение')
     # TODO Предусмотреть автоматический расчёт md5
     md5 = models.CharField(max_length=32)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
-    created_at = models.DateTimeField(auto_now=True)
-    note = models.CharField(max_length=128, blank=True)
+    status = models.CharField(max_length=20,
+                              choices=STATUS_CHOICES,
+                              verbose_name='Статус')
+    created_at = models.DateTimeField(auto_now=True, verbose_name='Создан')
+    note = models.CharField(max_length=128,
+                            blank=True,
+                            verbose_name='Примечание')
 
     def get_documents(self):
         return self.objects.all()
@@ -126,12 +141,20 @@ class Adjustment(models.Model):
         (4, 'Устранение ошибок'),
         (5, 'Другие причины'),
     )
-    section = models.ForeignKey(Section, on_delete=models.CASCADE)
-    document = models.ForeignKey(Document, on_delete=models.CASCADE)
-    pages = models.CharField(max_length=512)
-    code = models.CharField(max_length=1, choices=CODE_CHOICES)
-    note = models.CharField(max_length=128, blank=True)
-    body = models.CharField(max_length=256)
+    section = models.ForeignKey(Section,
+                                on_delete=models.CASCADE,
+                                verbose_name='Раздел')
+    document = models.ForeignKey(Document,
+                                 on_delete=models.CASCADE,
+                                 verbose_name='Документ')
+    pages = models.CharField(max_length=512, verbose_name='Лист')
+    code = models.CharField(max_length=1,
+                            choices=CODE_CHOICES,
+                            verbose_name='Шифр')
+    note = models.CharField(max_length=128,
+                            blank=True,
+                            verbose_name='Примечания')
+    body = models.CharField(max_length=256, verbose_name='Содержание')
 
     def get_document_adjustments(self, selected_document_id):
         return self.objects.filter(document=selected_document_id)
@@ -142,13 +165,19 @@ class Adjustment(models.Model):
 
 
 class Remark(models.Model):
-    number = models.IntegerField()
-    section = models.ForeignKey(Section, on_delete=models.CASCADE)
-    expert = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateField(auto_now=True)
-    body = models.CharField(max_length=1024)
-    link = models.CharField(max_length=256, blank=True)
-    basis = models.CharField(max_length=512, blank=True)
+    number = models.IntegerField(verbose_name='№')
+    section = models.ForeignKey(Section,
+                                on_delete=models.CASCADE,
+                                verbose_name='Раздел')
+    expert = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               verbose_name='Эксперт')
+    date = models.DateField(auto_now=True, verbose_name='Дата')
+    body = models.CharField(max_length=1024, verbose_name='Содержание')
+    link = models.CharField(max_length=256, blank=True, verbose_name='Ссылка')
+    basis = models.CharField(max_length=512,
+                             blank=True,
+                             verbose_name='Основание')
 
     def get_project_remarks(self):
         return self.objects.all()
@@ -162,16 +191,23 @@ class Remark(models.Model):
 
 
 class Comment(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               verbose_name='Автор')
     recipient = models.ForeignKey(User,
                                   on_delete=models.CASCADE,
-                                  related_name='recipient')
-    document = models.ForeignKey(Document, on_delete=models.CASCADE)
+                                  related_name='recipient',
+                                  verbose_name='Получатель')
+    document = models.ForeignKey(Document,
+                                 on_delete=models.CASCADE,
+                                 verbose_name='Документ')
     reply_to = models.ForeignKey('self',
                                  on_delete=models.CASCADE,
-                                 related_name='parent')
-    created_at = models.DateTimeField(auto_now=True)
-    body = models.CharField(max_length=1024)
+                                 related_name='parent',
+                                 verbose_name='Переслать')
+    created_at = models.DateTimeField(auto_now=True,
+                                      verbose_name='Дата создания')
+    body = models.CharField(max_length=1024, verbose_name='Содержание')
 
     class Meta:
         verbose_name = 'Комментарий'
