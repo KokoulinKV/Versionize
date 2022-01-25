6e9695a8cd1c22bcc2cf1b0a48c561c8ffe1c001
Ilya Pankov <iluxaan@mail.ru>
2022-01-25 12:42:01 +0300

    feat: write js script for table style

--- static/js/style.js
+++ static/js/style.js
@@ -4,4 +4,14 @@ $(document).ready(function () {
 
     // Стилизация пока всех таблиц. В зависимости от количества их строк
     console.log($('#table'));
+    table_rows = $('#table').children() // Получаем список всех строк из таблицы
+    for (let i = 0; i < table_rows.length; i++) {
+        // четным присваиваем один стиль, нечетным другой
+        if (i % 2) {
+            table_rows[i].classList.add('form-table__table_even');
+        } else {
+            table_rows[i].classList.add('form-table__table_odd');
+        }
+    }
+    console.log(table_rows);
 });
