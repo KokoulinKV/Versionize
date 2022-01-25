4eace964525c0c324db24b804e81cb7bfa9bc3cb
Ilya Pankov <iluxaan@mail.ru>
2022-01-25 13:13:49 +0300

    refactor: hide not working content

--- static/js/style.js
+++ static/js/style.js
@@ -3,10 +3,10 @@
 $(document).ready(function () {
 
     // Стилизация пока всех таблиц. В зависимости от количества их строк
-    console.log($('#table'));
+    // console.log($('#table'));
     table_rows = $('#table').children() // Получаем список всех строк из таблицы
     for (let i = 0; i < table_rows.length; i++) {
-        // четным присваиваем один стиль, нечетным другой
+        // четным устанавливаем один стиль, нечетным другой
         if (i % 2) {
             table_rows[i].classList.add('form-table__table_even');
         } else {
