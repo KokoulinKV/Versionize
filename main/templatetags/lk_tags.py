14d57548ac71bb4cba5bc76eaddb9f7c938ce3c3
Ilya Pankov <iluxaan@mail.ru>
2022-01-26 15:53:23 +0300

    feat: write new template tag

--- main/templatetags/lk_tags.py
+++ main/templatetags/lk_tags.py
@@ -1,7 +1,9 @@
 from atexit import register
 import imp
+from urllib import request
 from django import template
 from main.models import Project
+from service.models import Notification
 
 register = template.Library()
 
@@ -13,3 +15,9 @@ def get_user_project(filter=None):
 
     else:
         return Project.objects.filter(admin_id=filter)
+
+@register.inclusion_tag('main/lk.html', takes_context=True)
+def show_notifications(context):
+    request_user = context['request'].user
+    notifications = Notification.objects.filter(to_user=request.user.id).exclude(user_has_seen=True).order_by('-date')
+    return {'notifications': notifications}
