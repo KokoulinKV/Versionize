c8491f612fbeda04a8a53c9c03b0758113b02fa6
Ilya Pankov <iluxaan@mail.ru>
2022-01-27 10:54:58 +0300

    refactor(not-pop.js): del file

--- static/js/not-pop.js
+++
@@ -1,6 +0,0 @@
-// let not = document.getElementById('not-pop')
-// let popmin = document.getElementById('pop-min')
-
-// not.addEventListener('click', (e) => {
-//     popmin.classList.toggle('pop-mini_open')
-// })
