2f07cb1becef39207f4c6f95bcddaf1d688356e5
Ilya Pankov <iluxaan@mail.ru>
2022-01-31 13:13:59 +0300

    style: del old comments

--- user/views.py
+++ user/views.py
@@ -14,33 +14,8 @@ class UserLoginView(LoginView):
     template_name = 'user/login.html'
 
     def get_success_url(self):
-        return reverse('main:index', args=(self.request.user.id,))
-
-# def login(request):
-#     try:
-#         user_error = False
-#         if request.method == 'POST':
-#             form = UserLoginForm(data=request.POST)
-#             email = request.POST['email']
-#             username = User.objects.get(email=email)
-#             password = request.POST['password']
-#             user = auth.authenticate(username=username, password=password)
-#             if user and user.is_active:
-#                 auth.login(request, user)
-#                 return HttpResponseRedirect(reverse('main:index', args=(user.id,)))
-#         else:
-#             form = UserLoginForm()
-#     except:
-#         form = UserLoginForm()
-#         user_error = True
-#     context = {'title': 'Login',
-#                'form': form,
-#                'user_error': user_error}
-#     return render(request, 'user/login.html', context)
+        return reverse('main:index', args=(self.request.user.id, ))
 
 
-# def logout(request):
-#     auth.logout(request)
-#     return HttpResponseRedirect(reverse('user:login'))
 class UserLogoutView(LogoutView):
     next_page = '/'
