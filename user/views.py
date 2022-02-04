cc55df7b3d83cc86ba7f1ab190ae2631d46326c4
AlexKovyazin <76786127+AlexKovyazin@users.noreply.github.com>
2022-02-04 11:50:32 +0300

    refactor: UserLoginView form_valid() was commented

--- user/views.py
+++ user/views.py
@@ -10,12 +10,12 @@ class UserLoginView(LoginView):
     form_class = UserLoginForm
     template_name = 'user/login.html'
 
-    def form_valid(self, form):
-        remember_me = form.cleaned_data['remember_me']
-        login(self.request, form.get_user())
-        if remember_me:
-            self.request.session.set_expiry(1209600)
-        return super(LoginView, self).form_valid(form)
+#    def form_valid(self, form):
+#        remember_me = form.cleaned_data['remember_me']
+#        login(self.request, form.get_user())
+#        if remember_me:
+#            self.request.session.set_expiry(1209600)
+#        return super(LoginView, self).form_valid(form)
 
     def get_success_url(self):
         return reverse('main:index', args=(self.request.user.id, ))
