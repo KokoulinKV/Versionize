ac5e9282180fc40d26b66085148fe351b11f52a3
TheSleepyNomad <iluxaan@mail.ru>
2022-02-12 15:04:36 +0300

    style: add style for our commits

--- static/js/style.js
+++ static/js/style.js
@@ -1,9 +1,11 @@
+// * - Ответственный
+// ? - Что делает/За что отвечает
 // В этом файле будут храниться и запускаться функции, которые влияют на стилизацию и анимацию
 // Для применения стилей нам надо дождаться загрузки DOM
 $(document).ready(function () {
 
-    // @TheSleepyNomad
-    // Стилизация четных и нечетных строк
+    // * @TheSleepyNomad
+    // ? Стилизация четных и нечетных строк
     table_rows = $('#table').children() // Получаем список всех строк из таблицы
     for (let i = 0; i < table_rows.length; i++) {
 
@@ -19,8 +21,8 @@ $(document).ready(function () {
         };
     };
 
-    // @TheSleepyNomad
-    // Отображение определенного количества строк
+    // * @TheSleepyNomad
+    // ? Отображение определенного количества строк
     // Так как у нас пока одна таблица, то вешаем на все кнопки
     $('#tableSelect').change(function () {
 
@@ -62,9 +64,9 @@ $(document).ready(function () {
         };
     });
 
-    // @TheSleepyNomad
-    // Поиск по таблице
-    // Получаем поле для ввода текса
+    // * @TheSleepyNomad
+    // ? Поиск по таблице
+    //   Получаем поле для ввода текса
     $('#tableSearch').keyup(function () { // функция запускаеться каждый раз, когда будет отждата клавиша
         _this = this;
 
@@ -82,77 +84,77 @@ $(document).ready(function () {
 
 
 
-    // @TheSleepyNomad
-    // Переход через select
+    // * @TheSleepyNomad
+    // ? Переход через select
     $('#selectProject').change(function(){
 		window.location.href = $('option:selected',this).data('url');
 	});
 
-    // @TheSleepyNomad
-    // Открытие/закрытие бокового навигационного меню через кнопку меню и поле поиска
+    // * @TheSleepyNomad
+    // ? Открытие/закрытие бокового навигационного меню через кнопку меню и поле поиска
     $('#menu-btn, #searchBtn').click(function(){
         $('#sidebar').toggleClass('sidebar_active');
     });
 
-    // Скрипты для pop-up окон
+    // ? Скрипты для pop-up окон
 
-    // @TheSleepyNomad
-    // Для вызова pop-up формы уведомлений
+    // * @TheSleepyNomad
+    // ? Для вызова pop-up формы уведомлений
     $('#not-pop').click(function(){
         $('#pop-min').toggleClass('pop-mini_open');
     });
 
-    // @TheSleepyNomad
-    // Для вызова pop-up формы добавления документа
+    // * @TheSleepyNomad
+    // ? Для вызова pop-up формы добавления документа
     $('#add-document, #popup__close_doc').click(function(){
         $('#popup-document').toggleClass('popup_open');
     });
 
-    // @TheSleepyNomad
-    // Для вызова pop-up формы добавления раздела
+    // * @TheSleepyNomad
+    // ? Для вызова pop-up формы добавления раздела
     $('#add-section, #popup__close_sec').click(function(){
         $('#popup-section').toggleClass('popup_open');
     });
 
-    // @TheSleepyNomad
-    // Для вызова pop-up формы добавления проекта
+    // * @TheSleepyNomad
+    // ? Для вызова pop-up формы добавления проекта
     $('#add-project, #popup__close_pro').click(function(){
         $('#popup-project').toggleClass('popup_open');
     });
 
-    // @TheSleepyNomad
-    // Для вызова pop-up формы добавления корректировок
+    // * @TheSleepyNomad
+    // ? Для вызова pop-up формы добавления корректировок
     $('#add-remarkdoc, #popup__close_remarkdoc').click(function(){
         $('#popup-remarkdoc').toggleClass('popup_open');
     });
 
-    // @TheSleepyNomad
-    // Для вызова pop-up формы добавления корректировок
+    // * @TheSleepyNomad
+    // ? Для вызова pop-up формы добавления корректировок
     $('#secRefAdd, #subSecAdd, #newform-close').click(function(e){
         e.preventDefault()
         $('#newForm').toggleClass('new-form_open');
     });
 
-    // @KokoulinKV
-    // Для вызова pop-up формы изменения пароля
+    // * @KokoulinKV
+    // ? Для вызова pop-up формы изменения пароля
     $('#change-password, #popup__close_password').click(function(){
         $('#popup-password').toggleClass('popup_open');
     });
 
-    // @KokoulinKV
-    // Для вызова pop-up формы изменения фотографии
+    // * @KokoulinKV
+    // ? Для вызова pop-up формы изменения фотографии
     $('#change-photo, #popup__close_photo').click(function(){
         $('#popup-photo').toggleClass('popup_open');
     });
 
-    // @KokoulinKV
-    // Для вызова pop-up формы изменения email и phone
+    // * @KokoulinKV
+    // ? Для вызова pop-up формы изменения email и phone
     $('#change-email, #popup__close_email').click(function(){
         $('#popup-email').toggleClass('popup_open');
     });
 
-    // @AlexKovyazin
-    // Для вызова pop-up формы создания ИУЛа
+    // * @AlexKovyazin
+    // ? Для вызова pop-up формы создания ИУЛа
     $('#get-info-card, #popup__close_info_card').click(function(){
         $('#popup-info-card').toggleClass('popup_open');
     });
@@ -161,8 +163,8 @@ $(document).ready(function () {
         }
     );
 
-    // @AlexKovyazin
-    // Для вызова pop-up формы создания ИУЛа
+    // * @AlexKovyazin
+    // ? Для вызова pop-up формы создания ИУЛа
     $('#get-permission-card, #popup__close_permission_card').click(function(){
         $('#popup-permission-card').toggleClass('popup_open');
     });
@@ -171,8 +173,8 @@ $(document).ready(function () {
         }
     );
 
-    // @KokoulinKV
-    // Для вызова pop-up ошибок форм
+    // * @KokoulinKV
+    // ? Для вызова pop-up ошибок форм
     $('#popup__close_error').click(function(){
         $('#popup-error').toggleClass('popup_open');
     });
