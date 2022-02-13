1d1b998ded0c19d5497bf79b29708b65955e505f
TheSleepyNomad <iluxaan@mail.ru>
2022-02-13 17:26:01 +0300

    feat: write js for todoList when task checked

--- static/js/todoList.js
+++ static/js/todoList.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
+    $('.to-do-list__check-widget').on('click', function (e) {
+        let todoTitle = $(this).parent().parent().find('.to-do-list__title');
+        let todoText = $(this).parent().parent().find('.to-do-list__text');
 
+        if ($(this).is(':checked')) {
+            todoTitle.css('text-decoration', 'line-through');
+            todoText.css('text-decoration', 'line-through');
+        } else {
+            todoTitle.css('text-decoration', 'none');
+            todoText.css('text-decoration', 'none');
+        }
+    });
 });
